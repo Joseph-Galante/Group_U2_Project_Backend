c500585097b5659c2925d2359a78f4c8fc1fc137
Gregory Bertrand <gregorybertrand@Gregorys-MacBook-Pro.local>
2021-04-14 16:42:30 -0500

    routes

--- controllers/businessController.js
+++ controllers/businessController.js
@@ -2,49 +2,6 @@ const models = require('../models')
 
 const businessController = {}
 
-businessController.create = async (req, res) => {
-    try {
-        let newBusiness = await models.business.create({
-            name: req.body.name,
-            address: req.body.address,
-            type: req.body.type,
-            description: req.body.description,
-            image: req.body.image
-        })
-        res.json({newBusiness})
-    } catch (error) {
-        res.json({error})
-    }
-}
-
-businessController.update = async (req, res) => {
-    try {
-        let updates = req.body
-        let updatedBusiness = await models.business.findOne({
-            where: {
-                id: req.params.id
-            }
-        })
-
-        let changedBusiness = await updatedBusiness.update(updates)
-        res.json({changedBusiness})
-    } catch(error) {
-        res.json({error})
-    }
-}
-
-businessController.delete = async (req, res) => {
-    try {
-        let closedBusiness = await models.business.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-        res.json({closedBusiness})
-    } catch (error) {
-        res.json({error})
-    }
-}
 
 businessController.find = async (req, res) => {
     try {
@@ -53,7 +10,7 @@ businessController.find = async (req, res) => {
                 id: req.params.id
             }
         })
-        res.json({dino})
+        res.json({business})
     } catch(error) {
         res.json({error})
     }
@@ -64,6 +21,23 @@ businessController.findAll = async (req, res) => {
         let businesses = await models.business.findAll()
         res.json({businesses})
     } catch(error) {
+        res.json({error})
+    }
+}
+
+businessController.getAllReviews = async (req, res) => {
+    try {
+        let reviewSearch = await models.business.findOne({
+
+            where: {
+                id: req.params.id
+            }
+
+        })
+        
+        const reviews = await reviewSearch.getReviews()
+        res.json({reviews})
+    } catch (error) {
         res.json({error})
     }
 }
