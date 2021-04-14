3a1aea9ada310805e8aedb15a4bab2478136ddc4
Joe <jgalante00@gmail.com>
2021-04-14 16:32:36 -0400

    user controller

---
+++ routes/userRoutes.js
@@ -0,0 +1,18 @@
+// grab controller
+const userController = require('../controllers/userController');
+
+// express
+const express = require('express');
+const userRoutes = express.Router();
+
+// routes
+userRoutes.get('/', userController.getAll);
+userRoutes.post('/', userController.create);
+userRoutes.post('/login', userController.login);
+userRoutes.get('/profile', userController.profile);
+userRoutes.put('/profile', userController.update);
+userRoutes.post('/recipes', userController.saveRecipe);
+userRoutes.get('/recipes', userController.getSavedRecipes);
+userRoutes.delete('/recipes', userController.deleteSavedRecipe);
+
+module.exports = userRoutes;
