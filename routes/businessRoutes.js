bbd4100d24179cb8bd545e04f77bea55de52f793
Joe <jgalante00@gmail.com>
2021-04-14 16:49:16 -0400

    cors install, route fix

---
+++ routes/businessRoutes.js
@@ -0,0 +1,10 @@
+// grab controller
+const businessController = require('../controllers/businessController');
+
+// express
+const express = require('express');
+const businessRoutes = express.Router();
+
+// routes
+
+module.exports = businessRoutes;
